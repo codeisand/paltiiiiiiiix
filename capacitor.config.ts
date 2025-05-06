32e23137ab76796cf0e80030d14d96ae4d840bd4
guucode007 <app.code007@gmail.com>
2025-05-06 10:28:18 +0530

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'codeisand.paltics.app',
+  appName: 'paltics',
+  webDir: 'www'
+};
+
+export default config;
